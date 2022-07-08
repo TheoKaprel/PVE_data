39bc6d8d4193874cb978d5669b41e16d16fe5fa3
TheoKaprel <theo.kaprelian@creatis.insa-lyon.fr>
2022-07-08 10:48:26 +0200

    apply_poisson noise

---
+++ Analytical_data/apply_poisson_noise.py
@@ -0,0 +1,38 @@
+import click
+import itk
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+@click.command(context_settings=CONTEXT_SETTINGS)
+@click.option('--projection', '-p', required = True)
+@click.option('--output', '-o', required = True)
+def apply_poisson_noise_click(projection, output):
+    apply_poisson_noise(projection, output)
+
+
+
+
+def apply_poisson_noise(projection, output):
+    print(f'Opening {projection}...')
+
+    projection_itk = itk.imread(projection)
+    vSpacing = np.array(projection_itk.GetSpacing())
+    vOffset = np.array(projection_itk.GetOrigin())
+
+    projection_array = itk.array_from_image(projection_itk)
+
+
+    noisy_projection_array = np.random.poisson(lam = projection_array, size = projection_array.shape).astype(float)
+
+    noisy_projection_image = itk.image_from_array(noisy_projection_array)
+    noisy_projection_image.SetSpacing(vSpacing)
+    noisy_projection_image.SetOrigin(vOffset)
+    itk.imwrite(noisy_projection_image,output)
+
+    print(f'Done! Output at {output}')
+
+
+if __name__=='__main__':
+    apply_poisson_noise_click()
