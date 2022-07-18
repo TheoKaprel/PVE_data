2f992b325fcb6a991e2cd8bde1e1a98b9bf59776
TheoKaprel <theo.kaprelian@creatis.insa-lyon.fr>
2022-07-18 18:06:23 +0200

    full_spect rot

--- Simu_data/GAM_GATE/full_spect.py
+++ Simu_data/GAM_GATE/full_spect.py
@@ -6,18 +6,23 @@ import gam_gate as gam
 import contrib.spect_ge_nm670 as gam_spect
 
 
-
-
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 @click.command(context_settings=CONTEXT_SETTINGS)
 @click.option('--activity', '-a', 'upactivity', type = int, default = 1)
+@click.option('--rot', is_flag = True, default = False)
+@click.option('--nproj', '-n', type = int, default = 60)
 @click.option('--visu', is_flag = True, default = False)
 @click.option('--debug', 'updebug', is_flag = True, default = False)
-def sim_full_spect(upactivity, visu, updebug):
+@click.option('--output_folder', '-o')
+def sim_full_spect(upactivity,rot, nproj, visu, updebug, output_folder):
     paths = Box()
     paths.pwd = Path(os.getcwd())
     paths.data = paths.pwd / 'data'
-    paths.output = paths.pwd / 'outputs'
+
+    if output_folder:
+        paths.output = paths.pwd / 'outputs' / output_folder
+    else:
+        paths.output = paths.pwd / 'ouputs'
 
     sim = gam.Simulation()
 
@@ -28,13 +33,6 @@ def sim_full_spect(upactivity, visu, updebug):
     ui.visu_versbose = True
     ui.number_of_threads = 1
     ui.check_volumes_overlap = False
-
-    # units
-    m = gam.g4_units('m')
-    cm = gam.g4_units('cm')
-    keV = gam.g4_units('keV')
-    mm = gam.g4_units('mm')
-    Bq = gam.g4_units('Bq')
 
     # world size
     world = sim.world
@@ -47,7 +45,83 @@ def sim_full_spect(upactivity, visu, updebug):
     pos = [0, 0, -(sdd + psd)]
     spect.translation, spect.rotation = gam.get_transform_orbiting(pos, 'y', 0)
 
+    make_physics_list(sim)
 
+    activity = upactivity * Bq
+    source = sim.add_source('Voxels', 'source')
+    source.mother = world.name
+    source.particle = 'gamma'
+    source.activity = activity / ui.number_of_threads
+    source.image = paths.data / 'my_iec_test.mhd'
+    source.direction.type = 'iso'
+    source.energy.mono = 140.5 * keV
+    source.direction.acceptance_angle.volumes = ['spect']
+    source.direction.acceptance_angle.intersection_flag = True
+
+
+    # add stat actor
+    stats = sim.add_actor('SimulationStatisticsActor', 'Stats')
+    stats.track_types_flag=True
+
+    """ 
+        The order of the actors is important !
+        1. Hits
+        2. Singles
+        3. EnergyWindows
+    """
+    # hits collection
+    hc = sim.add_actor('HitsCollectionActor', 'Hits')
+    l = sim.get_all_volumes_user_info()
+    crystal = l[[k for k in l if 'crystal' in k][0]]
+    hc.mother = crystal.name
+    hc.output = paths.output / 'full_spect_hc.root'
+    hc.attributes = ['PostPosition', 'TotalEnergyDeposit', 'TrackVolumeCopyNo','PreStepUniqueVolumeID', 'PostStepUniqueVolumeID','GlobalTime', 'KineticEnergy', 'ProcessDefinedStep']
+    # singles collection
+    sc = sim.add_actor('HitsAdderActor', 'Singles')
+    sc.mother = crystal.name
+    sc.input_hits_collection = 'Hits'
+    sc.policy = 'EnergyWinnerPosition'
+    sc.skip_attributes = ['ProcessDefinedStep', 'KineticEnergy']
+    sc.output = hc.output
+    # EnergyWindows
+    cc = sim.add_actor('HitsEnergyWindowsActor', 'EnergyWindows')
+    cc.mother = crystal.name
+    cc.input_hits_collection = 'Singles'
+    cc.channels = [{'name': 'scatter', 'min': 114 * keV, 'max': 126 * keV},
+                   {'name': 'peak140', 'min': 126 * keV, 'max': 154.55 * keV},
+                   {'name': 'spectrum', 'min': 0 * keV, 'max': 5000 * keV}  # should be strictly equal to 'Singles'
+                   ]
+    cc.output = hc.output
+    # Projection
+    proj = sim.add_actor('HitsProjectionActor', 'Projection')
+    proj.mother = crystal.name
+    proj.input_hits_collections = ['spectrum', 'scatter', 'peak140']
+    proj.spacing = [4.41806 * mm, 4.41806 * mm]
+    proj.size = [128, 128]
+    # proj.plane = 'XY' # not implemented yet
+    proj.output = paths.output / 'proj.mhd'
+
+    if rot:
+        # motion of the spect, create also the run time interval
+        motion = sim.add_actor('MotionVolumeActor', 'Move')
+        motion.mother = spect.name
+        sim.run_timing_intervals = []
+        arc = 360
+        motion.translations, motion.rotations = gam.volume_orbiting_transform('y', 0, arc, nproj, spect.translation, spect.rotation)
+        motion.priority = 5
+
+        sim.run_timing_intervals = gam.range_timing(0, 1 * sec, nproj)
+        print(f'Run intervals: {sim.run_timing_intervals}')
+
+    sim.initialize()
+    sim.start()
+
+    stats = sim.get_actor('Stats')
+    print(stats)
+    stats.write(paths.output / 'stats.txt')
+
+
+def make_physics_list(sim):
     # physic list
     p = sim.get_physics_user_info()
     p.physics_list_name = 'G4EmStandardPhysics_option4'
@@ -61,88 +135,18 @@ def sim_full_spect(upactivity, visu, updebug):
     cuts.spect.electron = 0.01 * mm
     cuts.spect.positron = 0.1 * mm
 
-    # default source for tests
-    # activity = upactivity * Bq
-    # beam1 = sim.add_source('Generic', 'beam1')
-    # beam1.mother = world.name
-    # beam1.particle = 'gamma'
-    # beam1.energy.mono = 140.5 * keV
-    # beam1.position.type = 'sphere'
-    # beam1.position.radius = 3 * cm
-    # beam1.position.translation = [0, 0, 0 * cm]
-    # beam1.direction.type = 'momentum'
-    # beam1.direction.type = 'iso'
-    # beam1.activity = activity / ui.number_of_threads
 
-    activity = upactivity * Bq
-    source = sim.add_source('Voxels', 'source')
-    source.mother = world.name
-    source.particle = 'gamma'
-    source.activity = activity / ui.number_of_threads
-    source.image = paths.data / 'my_iec_test.mhd'
-    # source.direction.type = 'iso'
-    source.direction.type = 'momentum'
-    source.direction.momentum = [0,0,-1]
-    source.energy.mono = 140.5 * keV
-
-
-
-    # add stat actor
-    stats = sim.add_actor('SimulationStatisticsActor', 'Stats')
-    stats.track_types_flag=True
-
-
-    # hits collection
-    hc = sim.add_actor('HitsCollectionActor', 'Hits')
-    # get crystal volume by looking for the word crystal in the name
-    l = sim.get_all_volumes_user_info()
-    crystal = l[[k for k in l if 'crystal' in k][0]]
-    hc.mother = crystal.name
-    hc.output = paths.output / 'full_spect_hc.root'
-    hc.attributes = ['PostPosition', 'TotalEnergyDeposit', 'TrackVolumeCopyNo','PreStepUniqueVolumeID', 'PostStepUniqueVolumeID','GlobalTime', 'KineticEnergy', 'ProcessDefinedStep']
-
-    # singles collection
-    sc = sim.add_actor('HitsAdderActor', 'Singles')
-    sc.mother = crystal.name
-    sc.input_hits_collection = 'Hits'
-    sc.policy = 'EnergyWinnerPosition'
-    sc.skip_attributes = ['ProcessDefinedStep', 'KineticEnergy']
-    sc.output = hc.output
-
-    # EnergyWindows
-    cc = sim.add_actor('HitsEnergyWindowsActor', 'EnergyWindows')
-    cc.mother = crystal.name
-    cc.input_hits_collection = 'Singles'
-    cc.channels = [{'name': 'scatter', 'min': 114 * keV, 'max': 126 * keV},
-                   {'name': 'peak140', 'min': 126 * keV, 'max': 154.55 * keV},
-                   {'name': 'spectrum', 'min': 0 * keV, 'max': 5000 * keV}  # should be strictly equal to 'Singles'
-                   ]
-    cc.output = hc.output
-    """ 
-        The order of the actors is important !
-        1. Hits
-        2. Singles
-        3. EnergyWindows
-    """
-
-    proj = sim.add_actor('HitsProjectionActor', 'Projection')
-    proj.mother = crystal.name
-    proj.input_hits_collections = ['spectrum', 'scatter', 'peak140']
-    proj.spacing = [4.41806 * mm, 4.41806 * mm]
-    proj.size = [128, 128]
-    # proj.plane = 'XY' # not implemented yet
-    proj.output = paths.output / 'proj.mhd'
-
-
-    sim.initialize()
-    sim.start()
-
-    stats = sim.get_actor('Stats')
-    print(stats)
-    stats.write(paths.output / 'stats.txt')
-
-
+# units
+m = gam.g4_units('m')
+cm = gam.g4_units('cm')
+keV = gam.g4_units('keV')
+mm = gam.g4_units('mm')
+Bq = gam.g4_units('Bq')
+sec = gam.g4_units('second')
 
 
 if __name__=='__main__':
     sim_full_spect()
+
+
+
