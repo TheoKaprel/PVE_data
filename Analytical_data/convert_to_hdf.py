ff346c4e939b352f1f4b9bc15ea818762c53959a
TheoKaprel <theo.kaprelian@creatis.insa-lyon.fr>
2024-01-30 19:19:43 +0100

    handle mhd/mha data to convert to hdf

--- Analytical_data/convert_to_hdf.py
+++ Analytical_data/convert_to_hdf.py
@@ -4,26 +4,28 @@ import glob
 
 import numpy as np
 import argparse
+import itk
 
 
 def convert():
     print(args)
     keys=args.keys.split(',')
     print(keys)
+    ext_typ=args.type
 
-    list_frstkey = glob.glob(os.path.join(args.folder, f"?????_{keys[0]}.npy"))
+    list_frstkey = glob.glob(os.path.join(args.folder, f"?????_{keys[0]}.{ext_typ}"))
     f = h5py.File(os.path.join(args.folder, args.output), 'a')
     list_keys=list(f.keys())
     print(list_frstkey)
     for i,fn_src in enumerate(list_frstkey):
-        ref = fn_src.split(f'_{keys[0]}.npy')[0][-5:]
+        ref = fn_src.split(f'_{keys[0]}.{ext_typ}')[0][-5:]
         # if ref not in f_keys:
         print(ref)
 
         save = True
         for key in keys:
-            if not os.path.isfile(os.path.join(args.folder, f"{ref}_{key}.npy")):
-                print(f"{ref}_{key}.npy not in {args.folder}")
+            if not os.path.isfile(os.path.join(args.folder, f"{ref}_{key}.{ext_typ}")):
+                print(f"{ref}_{key}.{ext_typ} not in {args.folder}")
                 save=False
 
         if save==False:
@@ -36,14 +38,17 @@ def convert():
                 grp = f[ref]
 
             for key in keys:
-                save_key_in_grp(grp=grp,ref=ref,key=key,folder=args.folder)
+                save_key_in_grp(grp=grp,ref=ref,key=key,folder=args.folder,ext_typ=ext_typ)
 
     print('done!')
 
 
-def save_key_in_grp(grp,ref,key,folder):
-    array_fn = os.path.join(folder, f"{ref}_{key}.npy")
-    array = np.load(array_fn)
+def save_key_in_grp(grp,ref,key,folder,ext_typ):
+    array_fn = os.path.join(folder, f"{ref}_{key}.{ext_typ}")
+    if ext_typ!="npy":
+        array = itk.array_from_image(itk.imread(array_fn))
+    else:
+        array = np.load(array_fn)
     dset_key = grp.create_dataset(key, array.shape, dtype='float16')
     dset_key[:, :, :] = array
 
@@ -52,6 +57,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--folder')
     parser.add_argument('--keys', type=str)
+    parser.add_argument('--type', type=str)
     parser.add_argument('--output')
     args = parser.parse_args()
     convert()
