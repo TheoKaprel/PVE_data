f35e986210d727750e882960246bfaa43ed2e357
TheoKaprel <theo.kaprelian@creatis.insa-lyon.fr>
2024-02-14 13:23:39 +0100

    helpers

---
+++ Simu_data/opengate/gaga_garf_torch/gaga_garf/helpers.py
@@ -0,0 +1,38 @@
+import scipy
+import numpy as np
+
+def update_ideal_recons(batch,recons,offset,spacing,size,e_min=0.001):
+    batch=batch.cpu().numpy()
+    c = scipy.constants.speed_of_light * 1000  # in mm
+
+    # loop on x ; check energy
+    positions = batch[:,1:4]
+    directions = batch[:,4:7]
+
+    times = batch[:,7] / 1e9
+    energies = batch[:,0]
+
+    # filter according to E ?
+    # mask = energies > e_min
+    mask = (energies>0.140) & (energies<0.141)
+    positions = positions[mask]
+    directions = directions[mask]
+    times = times[mask]
+
+    # output
+    emissions = np.zeros_like(positions)
+
+    for pos, dir, t, E, p in zip(positions, directions, times, energies, emissions):
+        l = t * c
+        p += pos + l * -dir
+
+    pix = np.rint((emissions - offset) / spacing).astype(int)
+
+    size=[size[2],size[1],size[0]]
+    for i in [0, 1, 2]:
+        pix = pix[(pix[:, i] < size[i]) & (pix[:, i] > 0)]
+
+    for x in pix:
+        recons[x[2], x[1], x[0]] += 1
+
+    return recons
--- Simu_data/opengate/gaga_garf_torch/gaga_garf_torch.py
+++ Simu_data/opengate/gaga_garf_torch/gaga_garf_torch.py
@@ -9,47 +9,12 @@ from box import Box
 import torch
 import time
 import itk
-import scipy
 from tqdm import tqdm
 from gaga_garf.cgan_source import CGANSOURCE,ConditionsDataset
 from gaga_garf.garf_detector import GARF,DetectorPlane
+from gaga_garf import helpers
 
 
-def update_ideal_recons(batch,recons,offset,spacing,size,e_min=0.001):
-    batch=batch.cpu().numpy()
-    c = scipy.constants.speed_of_light * 1000  # in mm
-
-    # loop on x ; check energy
-    positions = batch[:,1:4]
-    directions = batch[:,4:7]
-
-    times = batch[:,7] / 1e9
-    energies = batch[:,0]
-
-    # filter according to E ?
-    # mask = energies > e_min
-    mask = (energies>0.140) & (energies<0.141)
-    positions = positions[mask]
-    directions = directions[mask]
-    times = times[mask]
-
-    # output
-    emissions = np.zeros_like(positions)
-
-    for pos, dir, t, E, p in zip(positions, directions, times, energies, emissions):
-        l = t * c
-        p += pos + l * -dir
-
-    pix = np.rint((emissions - offset) / spacing).astype(int)
-
-    size=[size[2],size[1],size[0]]
-    for i in [0, 1, 2]:
-        pix = pix[(pix[:, i] < size[i]) & (pix[:, i] > 0)]
-
-    for x in pix:
-        recons[x[2], x[1], x[0]] += 1
-
-    return recons
 
 def main():
     t0 = time.time()
@@ -143,7 +108,7 @@ def main():
             t_selection += (time.time() - t_selection_0)
 
             if args.save:
-                recons_generated = update_ideal_recons(batch=fake,
+                recons_generated = helpers.update_ideal_recons(batch=fake,
                                                        recons=recons_generated,
                                                        offset=offset[::-1],spacing=spacing,size=size,
                                                        e_min=0.140)
